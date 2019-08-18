d6ac10f714503ba724429b6f0c9764a7af768509
zesaianda <zesaianda@hotmail.com>
2019-08-18 23:08:05 +0100

    Terminando as funcionalidades do carrinho

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -4,6 +4,7 @@ import { CartItem } from '../../models/cart-item';
 import { ProdutoService } from '../../services/domain/produto.service';
 import { API_CONFIG } from '../../config/api.config';
 import { CartService } from '../../services/domain/cart.service';
+import { ProdutoDTO } from '../../models/produto.dto';
 
 
 @IonicPage()
@@ -40,4 +41,28 @@ export class CartPage {
     }
   }
 
+
+  removeItem(produto: ProdutoDTO) {
+    this.items = this.cartService.removeProduto(produto).items;
+  }
+
+
+  increaseQuantity(produto: ProdutoDTO) {
+    this.items = this.cartService.increaseQuantity(produto).items;
+  }
+
+
+  decreaseQuantity(produto: ProdutoDTO) {
+    this.items = this.cartService.decreaseQuantity(produto).items;
+  }
+
+
+  total() : number{
+    return this.cartService.total();
+  }
+
+
+  goOn() {
+    this.navCtrl.setRoot('CategoriasPage')
+  }
 }
