50e78fb9d66be7530f698000a8ee2ac5d0618302
zesaianda <zesaianda@hotmail.com>
2019-08-27 01:54:29 +0100

    Usando Cordova para tirar foto com a câmera

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-                                import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
