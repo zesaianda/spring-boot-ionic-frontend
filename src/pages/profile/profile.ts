2050f8922fc95dd3c418c6755bee782f4f385793
zesaianda <zesaianda@hotmail.com>
2019-09-25 16:01:56 +0100

    Bugfix - actualização da imagem de perfil

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -5,6 +5,7 @@ import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
 import { API_CONFIG } from '../../config/api.config';
 import { CameraOptions, Camera } from '@ionic-native/camera';
+import { DomSanitizer } from '@angular/platform-browser';
 
 /**
  * Generated class for the ProfilePage page.
@@ -21,13 +22,17 @@ import { CameraOptions, Camera } from '@ionic-native/camera';
 export class ProfilePage {
   cliente: ClienteDTO;
   picture: string;
+  profileImage;
   cameraOn: boolean = false;
 
   constructor(public navCtrl: NavController, 
               public navParams: NavParams,
               public storage: StorageService,
               public clienteService: ClienteService,
-              public camera: Camera) {
+              public camera: Camera,
+              public sanitizer: DomSanitizer) {
+      
+                this.profileImage = 'assets/imgs/avatar-blank.png';
   }
 
   ionViewDidLoad() {
@@ -58,9 +63,26 @@ export class ProfilePage {
     this.clienteService.getImageFromBucket(this.cliente.id)
     .subscribe(response => {
       this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+      this.blobToDataURL(response).then(dataUrl => {
+        let str : string =  dataUrl as string;
+        this.profileImage = this.sanitizer.bypassSecurityTrustUrl(str);
+      }) 
     },
-    error => {})
+    error => {
+      this.profileImage = 'assets/imgs/avatar-blank.png';
+    })
   }
+
+
+  blobToDataURL(blob) {
+    return new Promise((fulfill, reject) => {
+      let reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = (e) => fulfill(reader.result);
+      reader.readAsDataURL(blob);
+    });
+  }
+
 
 
   getCameraPicture() {
@@ -108,7 +130,7 @@ export class ProfilePage {
     this.clienteService.uploadPicture(this.picture)
     .subscribe(response => {
       this.picture = null;
-      this.loadData();
+      this.getImageIfExists();
     },
     error => {
     }) 
